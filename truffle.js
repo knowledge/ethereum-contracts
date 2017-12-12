9c354ced1a3b27fa0a0b35e3e4b6be75054530e7
Diego Barahona <diestrin@gmail.com>
2017-12-12 11:35:43 -0600

    update truffle config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*'
+    }
+  }
 };
