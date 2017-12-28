378dca55717acf8d6536dfb047356c1a416a03cf
Diego Barahona <diestrin@gmail.com>
2017-12-28 13:15:44 -0600

    add payment contract with tests

--- contracts/Migration.sol
+++ contracts/Migration.sol
@@ -2,4 +2,5 @@ pragma solidity ^0.4.17;
 
 import 'zeppelin/contracts/lifecycle/Migrations.sol';
 
+
 contract Migration is Migrations { }
--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,17 +1,3 @@
-const SafeMath = artifacts.require('SafeMath')
 const Knowledge = artifacts.require('Knowledge')
-const BasicToken = artifacts.require('BasicToken')
-const StandardToken = artifacts.require('StandardToken')
-const UpgradeableToken = artifacts.require('UpgradeableToken')
 
-module.exports = async (deployer) => {
-  await deployer.deploy(SafeMath)
-  await deployer.link(SafeMath, [BasicToken, UpgradeableToken])
-  await deployer.deploy(BasicToken)
-  await deployer.link(BasicToken, StandardToken)
-  await deployer.deploy(StandardToken)
-  await deployer.link(StandardToken, UpgradeableToken)
-  await deployer.deploy(UpgradeableToken)
-  await deployer.link(UpgradeableToken, Knowledge)
-  await deployer.deploy(Knowledge)
-}
+module.exports = deployer => deployer.deploy(Knowledge)
