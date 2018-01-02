3e933934f50034d0da47b03334bad0513184e229
gaboesquivel <contact@gaboesquivel.com>
2018-01-02 17:15:37 -0600

    configure script for ropsten deployment

--- truffle.js
+++ truffle.js
@@ -1,10 +1,21 @@
+require('dotenv').load()
+
+const HDWalletProvider = require("truffle-hdwallet-provider")
+
+const infuraToken = process.env.INFURA_TOKEN
+const mnemonic = process.env.MNEMONIC
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*'
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraToken}`),
+      gas: 67123900, // https://git.io/vbhSl
+      network_id: 3
     }
   },
   mocha: {
