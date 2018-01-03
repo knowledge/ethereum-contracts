62f5334a479770dd7e2403695d8bdeca075f52ee
Diego Barahona <diestrin@gmail.com>
2018-01-02 22:02:35 -0600

    add payable token

--- contracts/PayableToken.sol
+++ contracts/PayableToken.sol
@@ -9,11 +9,35 @@ import { ERC20Token } from './ERC20Token.sol';
  * order id or some other identifier
  */
 contract PayableToken is ERC20Token {
+  struct PaymentRequest {
+    uint256 value;
+    uint8 fee;
+    address seller;
+  }
+
+  mapping (address => mapping(string => PaymentRequest)) private pendingPayments;
+
   event Pay(address from, address to, uint256 amount, string ref);
 
-  function pay(address to, uint256 amount, string ref) public returns (bool) {
-    assert(transfer(to, amount));
-    Pay(msg.sender, to, amount, ref);
+  function requestPayment(uint256 value, uint8 fee, string ref, address to) public {
+    pendingPayments[msg.sender][ref] = PaymentRequest(value, fee, to);
+  }
+
+  function paymentInfo(address store, string ref) public view returns (uint256 value, uint8 fee, address seller) {
+    PaymentRequest memory paymentRequest = pendingPayments[store][ref];
+    value = paymentRequest.value;
+    fee = paymentRequest.fee;
+    seller = paymentRequest.seller;
+  }
+
+  function pay(address store, string ref) public returns (bool) {
+    PaymentRequest memory paymentRequest = pendingPayments[store][ref];
+
+    assert(transfer(store, paymentRequest.fee));
+    assert(transfer(paymentRequest.seller, paymentRequest.value));
+
+    Pay(msg.sender, paymentRequest.seller, paymentRequest.value, ref);
+
     return true;
   }
 }
